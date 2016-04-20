33e3518ebc46b5277c397753febc61a10fa09864
ayambean <603700716@qq.com>
2016-04-20 20:08:34 +0800

    主页面评论显示(味测试) 当前操作步骤显示(完成)

--- finalproject/js/index.js
+++ finalproject/js/index.js
@@ -8,24 +8,63 @@ var nowstage;
 
 function pageonload(){
 
-    var realurl =ip+"TradingSystem/loginview.action?";
+    var realurl1 =ip+"TradingSystem/loginview.action?";
 
 
     $.ajax({
         type: "POST",
         async:false,
-        url:realurl,
+        url:realurl1,
         data:{
             username:pageusername
         },
         dataType:"jsonp",
         jsonp:"callback",
+        jsonpCallback : "handler1",
         success:function (data) {
             //alert("data"+data.result);
             alert("stage"+data.stage);
             nowstage = data.stage;
             $("#laststagenumber").html("操作"+data.stage);
             $("#laststrequest").html(data.latestrequest);
+            //$("#latestfbk").html(data.latestfbk);
+        },
+        error:function () {
+            alert("AJAX请求失败");
+        }
+    })
+
+    var realurl2 =ip+"TradingSystem/evaluation.action?";
+    $.ajax({
+        type: "POST",
+        async:false,
+        url:realurl2,
+        data:{
+            stuname:pageusername
+        },
+        dataType:"jsonp",
+        jsonp:"callbackeva",
+
+        success:function (data) {
+            //alert("data"+data.result);
+            alert("stage"+data.stage);
+            $.each(data.commentlist, function(i, item) {
+                $("#maindianpinginsertpoint").append(
+                    // "<div>" + item.dostage + "</div>" +
+                    // "<div>" + item.dianping    + "</div>" +
+                    // "<div>" + item.time + "</div><hr/>");
+                "<li class='left clearfix'>"+
+                    "<span class='chat-img pull-left'>"+
+                    "<img src='photo/g8201306142323_jpg!article.jpg' alt='User Avatar' class='img-circle' style='height: 80px;width: 80px'/>"+
+                    "</span>"+
+                    "<div class='chat-body clearfix'>"+
+                    "<div class='header'>"+
+                    "<strong class='primary-font'>"+item.dostage+"</strong> <small class='text-muted'>"+item.time+"</small>"+
+                    "</div>"+
+                    "<p>"+item.dianping +"</p>"+
+                    "</div>"+
+                    "</li>")
+            });
             //$("#latestfbk").html(data.latestfbk);
         },
         error:function () {
@@ -63,11 +102,21 @@ function showmainpage(){
     $('#test').hide(2000);
     $('#test').hide(2000);
 
+    $('#ligradepage').attr("class", "");
+    $('#libussinesspage').attr("class", "");
+    $('#lisearchpage').attr("class", "");
+    $('#test').attr("class", "");
+    $('#test').attr("class", "");
+    $('#test').attr("class", "");
     $('#mainpage').show(2000);
+    $('#limainpage').attr("class", "active");
+
 
 }
 
 function showgradepage(){
+
+    gradepageonload();
 
     $('#mainpage').hide(2000);
     $('#bussinesspage').hide(2000);
@@ -76,7 +125,15 @@ function showgradepage(){
     $('#test').hide(2000);
     $('#test').hide(2000);
 
+    $('#limainpage').attr("class", "");
+    $('#libussinesspage').attr("class", "");
+    $('#lisearchpage').attr("class", "");
+    $('#test').attr("class", "");
+    $('#test').attr("class", "");
+    $('#test').attr("class", "");
+
     $('#gradepage').show(2000);
+    $('#ligradepage').attr("class", "active");
 
 }
 
@@ -88,7 +145,15 @@ function showsearchpage() {
     $('#test').hide(2000);
     $('#test').hide(2000);
 
+    $('#ligradepage').attr("class", "");
+    $('#libussinesspage').attr("class", "");
+    $('#limainpage').attr("class", "");
+    $('#litest').attr("class", "");
+    $('#litest').attr("class", "");
+    $('#litest').attr("class", "");
+
     $('#searchpage').show(2000);
+    $('#lisearchpage').attr("class", "active");
 }
 
 
@@ -105,9 +170,10 @@ function gradepageonload(){
     var realurl =ip+"TradingSystem/gradelist.action?";
     var step=1;//初始化步骤加载
     var arrayObj = new Array();　//创建一个数组存分数
-    alert("到这");
+    //alert("到这");
 
     $("#gradeheading").html("当前"+pageusername+"的成绩单");
+    $("#processheading").html("当前"+pageusername+"的进度表单");
 
     $.ajax({
         type: "POST",
@@ -118,9 +184,10 @@ function gradepageonload(){
         },
         dataType:"jsonp",
         jsonp:"callback",
+        jsonpCallback : "handler1",
         success:function (data) {
             //alert("data"+data.result);
-                alert("stage"+data.grade1);
+               // alert("stage"+data.grade1);
             arrayObj[1]=data.grade1;
             arrayObj[2]=data.grade2;
             arrayObj[3]=data.grade3;
@@ -139,8 +206,21 @@ function gradepageonload(){
             arrayObj[16]=data.grade16;
 
             for(var i=1;i<=nowstage;i++){
-                alert(arrayObj[i]);
+               // alert(arrayObj[i]);
                 $("#gradeinsertpoint").append("<tr> <td>操作"+i+"</td> <td>"+arrayObj[i]+"</td> </tr>");
+            }
+
+            for(var i=1;i<=16;i++){
+                if(i<nowstage){
+                    $("#processinsertpoint").append("<tr> <td>操作"+i+"</td> <td>已完成</td> </tr>");
+                }else if(i==nowstage){
+                    $("#processinsertpoint").append("<tr> <td>操作"+i+"</td> <td>正在进行</td> </tr>");
+                }else if(i>nowstage){
+                    $("#processinsertpoint").append("<tr> <td>操作"+i+"</td> <td>尚未完成</td> </tr>");
+                }
+
+
+
             }
 
 
@@ -172,6 +252,6 @@ $(document).ready(function () {
     alert("用户名："+pageusername);
     pageonload();
     nowtimeshow();
-    gradepageonload();
+
 
 });
